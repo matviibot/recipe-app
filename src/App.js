258e6495e172476ae934bb31e72d8279648879f4
matvii <matveybot@gmail.com>
2023-05-13 16:04:45 -0230

    Text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import {PrimaryButton, SecondaryButton, SuccessButton} from "./components/atoms/Button";
 import {PrimaryInput} from "./components/atoms/Input";
+import { PrimaryText, SecondaryText, DangerText, WarningText, InfoText, SuccessText, MutedText, WhiteText, BlackText, DarkText } from "./components/atoms/Text";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <PrimaryButton>Primary</PrimaryButton>
-        <SecondaryButton>Secondary</SecondaryButton>
-        <SuccessButton>Success</SuccessButton>
-        <PrimaryInput placeholder="Primary" />
-    </div>
-  );
+    return (
+        <div className="App ">
+            <h1>Text Variants</h1>
+            <PrimaryText>Primary Text</PrimaryText>
+            <SecondaryText>Secondary Text</SecondaryText>
+            <DangerText>Danger Text</DangerText>
+            <WarningText>Warning Text</WarningText>
+            <InfoText>Info Text</InfoText>
+            <SuccessText>Success Text</SuccessText>
+            <MutedText>Muted Text</MutedText>
+            <WhiteText>White Text</WhiteText>
+            <BlackText>Black Text</BlackText>
+            <DarkText>Dark Text</DarkText>
+
+            <h1>Input Variants</h1>
+            <PrimaryInput placeholder="Primary Input"/>
+
+            <h1>Button Variants</h1>
+            <PrimaryButton>Primary</PrimaryButton>
+            <SecondaryButton>Secondary</SecondaryButton>
+            <SuccessButton>Success</SuccessButton>
+            <PrimaryInput placeholder="Primary"/>
+        </div>
+    );
 }
 
 export default App;
