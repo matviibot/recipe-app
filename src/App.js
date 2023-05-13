d4b3cf0473ccc0df4a472ba65a713b689e6e12b0
matvii <matveybot@gmail.com>
2023-05-13 15:48:21 -0230

    Input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {PrimaryButton, SecondaryButton, SuccessButton} from "./components/atoms/Button";
+import {PrimaryInput} from "./components/atoms/Input";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <PrimaryButton>Primary</PrimaryButton>
-      <SecondaryButton>Secondary</SecondaryButton>
-      <SuccessButton>Success</SuccessButton>
+        <PrimaryButton>Primary</PrimaryButton>
+        <SecondaryButton>Secondary</SecondaryButton>
+        <SuccessButton>Success</SuccessButton>
+        <PrimaryInput placeholder="Primary" />
     </div>
   );
 }
