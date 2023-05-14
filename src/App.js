d2931436c7682d19928593600dec0f09512a7ccc
matvii <matveybot@gmail.com>
2023-05-14 09:59:32 -0230

    RecipeCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import {PrimaryButton, SecondaryButton, SuccessButton} from "./components/atoms/Button";
 import {PrimaryInput} from "./components/atoms/Input";
-import { PrimaryText, SecondaryText, DangerText, WarningText, InfoText, SuccessText, MutedText, WhiteText, BlackText, DarkText } from "./components/atoms/Text";
+import {
+    PrimaryText,
+    SecondaryText,
+    DangerText,
+    WarningText,
+    InfoText,
+    SuccessText,
+    MutedText,
+    WhiteText,
+    BlackText,
+    DarkText
+} from "./components/atoms/Text";
+import RecipeCard from "./components/molecules/RecipeCard";
 import './App.css';
 
 function App() {
@@ -25,7 +37,25 @@ function App() {
             <PrimaryButton>Primary</PrimaryButton>
             <SecondaryButton>Secondary</SecondaryButton>
             <SuccessButton>Success</SuccessButton>
-            <PrimaryInput placeholder="Primary"/>
+
+            <h1>Recipe Cards</h1>
+            <div className="row">
+                {(() => {
+                    const recipeCards = [];
+                    for (let i = 0; i < 12; i++) {
+                        recipeCards.push(
+                            <div className="col col-lg-2">
+                                <div className="d-flex justify-content-center">
+                                    <RecipeCard
+                                        image="https://static.onecms.io/wp-content/uploads/sites/43/2022/05/26/8805-CrispyFriedChicken-mfs-3x2-072.jpg"
+                                        title="Chikem" text="chikem recipe"/>
+                                </div>
+                            </div>
+                        );
+                    }
+                    return recipeCards;
+                })()}
+            </div>
         </div>
     );
 }
