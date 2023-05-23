69ea06bc19c2168347c1a38ec991ca0f649ce16b
matvii <matveybot@gmail.com>
2023-05-23 21:20:47 -0230

    submit search on enter

--- src/components/molecules/SearchBar/SearchBar.js
+++ src/components/molecules/SearchBar/SearchBar.js
@@ -1,31 +1,32 @@
-import React, {useState} from 'react';
-import {SearchInput} from "../../atoms/Input";
-import {PrimaryButton} from "../../atoms/Button";
+import React, { useState } from 'react';
+import { SearchInput } from "../../atoms/Input";
+import { PrimaryButton } from "../../atoms/Button";
 import styles from './SearchBar.module.css'
 
-const SearchBar = ({setRequest}) => {
-    const [value, setValue] = useState('')
+const SearchBar = ({ setRequest }) => {
+    const [value, setValue] = useState('');
+
     const handleInput = (e) => {
+        setValue(e.target.value);
+    };
 
-        setValue(e.target.value)
-    }
-    const handleSubmit = () => {
-        console.log(value)
-        setRequest(value)
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Prevent form submission and page refresh
+        setRequest(value);
+    };
+
     return (
-        <div className={'container d-flex justify-content-center'}>
-        <div className={styles['search-bar']}>
-            <div className="input-group">
-            <span className="input-group-text bg-dark text-light border-0">
-              <i className="bi bi-search"></i>
-            </span>
-                <SearchInput onSubmit={handleSubmit} onChange={e => handleInput(e)} value={value} placeholder="Search for recipes"/>
-                <PrimaryButton onClick={handleSubmit} >Search</PrimaryButton>
+        <form onSubmit={handleSubmit} className="container d-flex justify-content-center">
+            <div className={styles['search-bar']}>
+                <div className="input-group">
+          <span className="input-group-text bg-dark text-light border-0">
+            <i className="bi bi-search"></i>
+          </span>
+                    <SearchInput onChange={handleInput} value={value} placeholder="Search for recipes" />
+                    <PrimaryButton type="submit">Search</PrimaryButton>
+                </div>
             </div>
-
-            </div>
-        </div>
+        </form>
     );
 };
 
