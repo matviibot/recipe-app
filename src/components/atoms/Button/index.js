51a7c6fab9cf4b2082fe6e388a81ed740f66a210
matvii <matveybot@gmail.com>
2023-06-05 12:27:23 -0230

    dark-theme

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
 
-export const PrimaryButton = props => <Button {...props} type={props.type} className="btn btn-primary btn-dark" />;
+export const PrimaryButton = props => <Button {...props} type={props.type} className={"btn btn-primary "+props.className} />;
 export const SecondaryButton = props => <Button {...props} className="btn btn-secondary btn-dark" />;
 export const SuccessButton = props => <Button {...props} className="btn btn-primary btn-success" />;
--- src/components/molecules/RecipeCard/RecipeCard.js
+++ src/components/molecules/RecipeCard/RecipeCard.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {PrimaryButton} from "../../atoms/Button";
 
 const RecipeCard = ({image, title, type, toggleModal}) => {
-    return (<div className="card mb-4" style={{width: "18rem"}}>
+    return (<div className="card bg-dark mb-4 text-light" style={{width: "18rem"}}>
             <img src={image} className="card-img-top" alt="recipe img"/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="text-muted">{type.map(t=>t+" ")}</p>
-                <PrimaryButton type="submit" onClick={()=>toggleModal(title)} className="btn btn-primary">View Recipe</PrimaryButton>
+                <PrimaryButton type="submit" onClick={()=>toggleModal(title)} className={"btn-light"}>View Recipe</PrimaryButton>
             </div>
         </div>
 
